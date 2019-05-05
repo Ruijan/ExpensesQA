c0a69c283f59f543ed228630913dda0917317a25
JulienR <rechenmann@gmail.com>
2019-05-05 22:18:17 +0200

    SignIn Test

---
+++ src/SignInTest.php
@@ -0,0 +1,114 @@
+<?php
+namespace QA\SignIn;
+use PHPUnit\Framework\TestCase;
+class SignInTest extends TestCase
+{
+    private $baseURL = "https://pixelnos-ledger-api.herokuapp.com/BackEnd/index.php";
+    public function testSignInWithNoParameters(){
+        $url = $this->baseURL.'?action=connection/SignIn';
+        $data = array();
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $this->assertEquals("Missing parameters email, password in request SignIn", $result);
+    }
+
+    public function testSignInWithMissingPassword(){
+        $url = $this->baseURL.'?action=connection/SignIn';
+        $data = array('email' => 'value1');
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $this->assertEquals("Missing parameters password in request SignIn", $result);
+    }
+
+    public function testSignInWithMissingEmail(){
+        $url = $this->baseURL.'?action=connection/SignIn';
+        $data = array('password' => 'value1');
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $this->assertEquals("Missing parameters email in request SignIn", $result);
+    }
+
+    public function testSignInWithWrongCredentials(){
+        $url = $this->baseURL.'?action=connection/SignIn';
+        $data = array('email' => 'value1', 'password' => 'value1');
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $this->assertEquals("ERROR: Email or password invalid", $result);
+    }
+
+    public function testSignInWithCorrectCredentials(){
+        $this->signUp();
+        $url = $this->baseURL.'?action=connection/SignIn';
+        $data = array('email' => 'email@hotmail.com', 'password' => '123456789');
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $this->assertEquals("Connected", $result);
+    }
+
+    private function signUp(){
+        $url = $this->baseURL.'?action=connection/SignUp';
+        $data = array('email' => 'email@hotmail.com',
+            'password' => '123456789',
+            'first_name' => 'Julien',
+            'last_name' => 'Rechenmann');
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if($result == FALSE){
+            echo "wrong";
+        }
+    }
+}
